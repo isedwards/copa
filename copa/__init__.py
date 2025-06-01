aa1f3d2563748e03c001895c6811ea1d5d301cc8
isedwards <ian@myacorn.com>
2025-06-01 16:51:31 +0100

    Add toc.yml and update to copy conf with package install

--- copa/__init__.py
+++ copa/__init__.py
@@ -1,7 +1,5 @@
 """
-Please refer to the documentation provided in the README.md
-"""
+copa: Configure, Orchestrate and Provision Applications
 
-__author__ = """Ian Edwards"""
-__email__ = "ian@myacorn.com"
-__version__ = "0.1.0"
+See README.md for usage examples and documentation.
+"""
